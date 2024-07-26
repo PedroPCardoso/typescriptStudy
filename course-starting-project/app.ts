b70b4c92bb94a9bb84ebcf68c1f2a51b0c613bdc
Pedro Cardoso <pedroecomp@gmail.com>
2024-07-26 15:17:26 -0300

    Aula 12

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -3,3 +3,9 @@ console.log('Your code goes here...');
 function add(a: number, b: number) {
     return a + b;
 }
+
+const number1 =  5;
+const number2 =  5;
+
+const result = add(number1, number2);
+console.log(result);
