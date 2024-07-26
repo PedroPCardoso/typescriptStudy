037ef08878516396996ba2024b1472c35a694c2e
Pedro Cardoso <pedroecomp@gmail.com>
2024-07-26 19:39:43 -0300

    aula21 -  enum

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -3,18 +3,20 @@
 //     age: number;
 //     nickname: string;
 // }
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
 const person: {
     name: string;
     age: number;
     nickname: string;
-    hobbies: string[]
-    role: [number, string]
+    hobbies: string[],
+    role: Role
 } = {
     name: 'Max',
     age: 30,
     nickname: 'Maximilian',
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };  
 
 // person.role.push('admin'); // vai dar  certo, o push é uma excessão
@@ -22,5 +24,8 @@ const person: {
 
 for(const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
+}
+if(person.role === Role.ADMIN) {
+    console.log('is admin');
 }
 // console.log(person.name);
