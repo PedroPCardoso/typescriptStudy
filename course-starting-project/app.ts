e616e3fb2d2e62fbbeb3a8365aa214304de3017b
Pedro Cardoso <pedroecomp@gmail.com>
2024-07-26 16:05:52 -0300

    Aula 16

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,11 +1,17 @@
 console.log('Your code goes here...');
 
-function add(a: number, b: number) {
-    return a + b;
+function add(a: number, b: number,printResult: boolean,phrase: string) {
+    const result = a + b;
+    if(printResult){
+        console.log(phrase + result);
+    }
+    else{
+        return result;
+    }
 }
 
 const number1 =  5;
 const number2 =  5;
-
-const result = add(number1, number2);
-console.log(result);
+const printResult = true;
+const phrase = 'Result is: ';
+add(number1, number2, printResult, phrase);
