9c1900c7ea044b9dbee93c44e954fd10fdc1c52d
Pedro Cardoso <pedroecomp@gmail.com>
2024-08-13 09:44:14 -0300

    interfaces e optional params

--- section-05/src/app.ts
+++ section-05/src/app.ts
@@ -1,61 +1,23 @@
-class Departament {
-    private readonly id: number;
-    private name: string;
-    protected employees: string[] = [];
-    constructor(id: number, name: string) {
-        this.id = id;
-        this.name = name;
-    }
-    set setName(name: string) {
-        this.name = name;
-    }
-    set setEmployees(employees: string[]) {
-        this.employees = employees;
-    }
+interface Person{
+    name: string;
+    age: number;
 
-    
-    get getId() {
-        return this.id;
-    }
-
-    get getName() {
-        return this.name;
-    }
-
-    describe() {
-        console.log(`Department (${this.id}): ${this.name}`);
-    }
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
-
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
+    greet(phrase: string): void;
 }
 
-class ITDepartament extends Departament {
-    constructor(id: number, name: string, private admins: string[]) {
-        super(id, name);
-    }
-    addAdmin(admin:string){
-        this.admins.push(admin);
-    }
+interface PersonNormal{
+    name: string;
+    age?: number;
 
-    printAdmin() {
-        console.log(this.admins);
-    }
+    greet(phrase: string): void;
 }
 
-const accounting = new Departament(1, 'Accounting');
-const accountingTI = new ITDepartament(1, 'Accounting',['Max', 'Test']);
+let user: Person;
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Test');
-
-
-console.log(accounting);
-console.log(accounting.getName);
-
-accountingTI.printAdmin();
+user = {
+    name: 'Max',
+    age: 30,
+    greet(phrase: string){
+        console.log(phrase + ' ' + this.name);
+    }
+}
